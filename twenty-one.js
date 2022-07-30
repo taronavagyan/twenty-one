72d3b93951526cb4fb5a4b16b0e39fed1a438f5b
Taron Avagyan <contact@taronavagyan.com>
2022-07-29 17:08:41 -0700

    deal cards

--- twenty-one.js
+++ twenty-one.js
@@ -27,6 +27,10 @@ class Deck {
     "2",
   ];
   constructor() {
+    this.reset();
+  }
+
+  reset() {
     this.deck = [];
     for (let suitIdx = 0; suitIdx < 4; suitIdx += 1) {
       for (let rankIdx = 0; rankIdx < 13; rankIdx += 1) {
@@ -38,68 +42,56 @@ class Deck {
     shuffle(this.deck);
   }
 
-  deal() {
-    // STUB
-    // does the dealer or the deck deal?
+  getTopCard() {
+    return this.deck.shift();
   }
 }
 
 class Participant {
   constructor() {
+    this.balance = 5;
+    this.score = 0;
+    this.hand = [];
     // STUB
     // What sort of state does a participant need?
     // Score? Hand? Amount of money available?
     // What else goes here? All the redundant behaviors from Player and Dealer?
   }
+
+  addToHand(card) {
+    this.hand.push(card);
+  }
+
+  resetHand() {
+    this.hand = [];
+  }
+
+  hit() {
+    // STUB
+  }
+
+  stay() {
+    // STUB
+  }
+
+  isBusted() {
+    // STUB
+  }
+
+  score() {
+    // STUB
+  }
 }
 
 class Player extends Participant {
   constructor() {
-    // STUB
-    // What sort of state does a player need?
-    // Score? Hand? Amount of money available?
-  }
-
-  hit() {
-    // STUB
-  }
-
-  stay() {
-    // STUB
-  }
-
-  isBusted() {
-    // STUB
-  }
-
-  score() {
-    // STUB
+    super();
   }
 }
 
 class Dealer extends Participant {
-  // Very similar to a Player; do we need this?
-
   constructor() {
-    // STUB
-    // What sort of state does a dealer need?
-    // Score? Hand? Deck of cards? Bow tie?
-  }
-
-  hit() {
-    // STUB
-  }
-
-  stay() {
-    // STUB
-  }
-
-  isBusted() {
-    // STUB
-  }
-
-  score() {
-    // STUB
+    super();
   }
 
   hide() {
@@ -109,15 +101,14 @@ class Dealer extends Participant {
   reveal() {
     // STUB
   }
-
-  deal() {
-    // STUB
-    // does the dealer or the deck deal?
-  }
 }
 
 class TwentyOneGame {
   constructor() {
+    this.player = new Player();
+    this.dealer = new Dealer();
+    this.deck = new Deck();
+
     // STUB
     // What sort of state does the game need?
     // A deck? Two participants?
@@ -135,7 +126,10 @@ class TwentyOneGame {
   }
 
   dealCards() {
-    // STUB
+    for (let cardNum = 0; cardNum < 2; cardNum += 1) {
+      this.player.addToHand(this.deck.getTopCard());
+      this.dealer.addToHand(this.deck.getTopCard());
+    }
   }
 
   showCards() {
@@ -165,3 +159,6 @@ class TwentyOneGame {
 
 let game = new TwentyOneGame();
 game.start();
+
+console.log(game.player.hand);
+console.log(game.dealer.hand);
